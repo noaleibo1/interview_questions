e8d6844d85769d9010d15b52ed406a89b3d7995b
Noa Leibovich <noa.leibovich@here.com>
2020-06-09 16:49:13 +0300

    Find median of three - naiive

---
+++ medean_of_three/main/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	input := os.Args[1:]
+	intInput, err := covertToInt(input)
+	if err != nil {
+		panic(err.Error())
+	}
+	if !validInput(intInput) {
+		panic("Input should be 3 integers")
+	}
+
+	naiiveSolution(intInput)
+
+
+}
+
+func covertToInt(input []string) ([]int, error) {
+	var intArray []int
+	for _, s := range input {
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil, errors.New("some of the input is invalid")
+		}
+		intArray = append(intArray, i)
+	}
+	return intArray, nil
+}
+
+func validInput(input []int) bool {
+	if len(input) != 3 {
+		return false
+	}
+	return true
+}
---
+++ medean_of_three/main/naiive.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func secondIsMedian(input []int) bool {
+	return firstIsMedian([]int{input[1], input[0], input[2]})
+}
+
+func firstIsMedian(input []int) bool {
+	return checkIfMedian(input[0], input[1:])
+}
+
+func checkIfMedian(i int, array []int) bool {
+	return (i >= array[0] && i <= array[1]) || (i <= array[0] && i >= array[1])
+}
+
+func naiiveSolution(input []int) {
+	if firstIsMedian(input) {
+		fmt.Println(input[0])
+	} else if secondIsMedian(input) {
+		fmt.Println(input[1])
+	} else {
+		fmt.Println(input[2])
+	}
+}
