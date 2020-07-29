858315eaad726750596c08e57a4aec835c218dd3
Noa Leibovich <noa.leibovich@here.com>
2020-07-29 14:13:48 +0300

    Remove some folders

--- wix/consts.go
+++
@@ -1,9 +0,0 @@
-package main
-
-const (
-	ErrorTreeIsGraph = "This is a graph"
-	ErrorInvalidTree = "This is an invalid tree"
-
-	// source: https://codereview.stackexchange.com/questions/121924/regular-expression-matching-with-string-slice-in-go
-	RegexForDeserializationInput = `^\\w+?,\\s\\w+?,\\s\\w+(?:\\s\\w+)?,\\s\\d{8},\\s(?:\\d{3}\\s){2}\\d{4}$`
-)
